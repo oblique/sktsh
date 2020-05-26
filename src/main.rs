0107a19819cca7179b3fe24042d6c04e6a977f10
oblique <psyberbits@gmail.com>
2020-05-26 23:32:10 +0300

    Move `cmd_*.rs` in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,48 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 mod client;
-mod cmd_connect;
-mod cmd_listen;
 mod errors;
 mod pty;
 mod raw_term;
 mod server;
 
-use crate::cmd_connect::cmd_connect;
-use crate::cmd_listen::cmd_listen;
+use crate::client::Client;
+use crate::server::Server;
 
 #[derive(Debug, StructOpt)]
 enum Opts {
     /// Start server
-    Listen {
-        /// Socket path
-        path: PathBuf,
-    },
-
+    Listen(ListenOpts),
     /// Connect to server
-    Connect {
-        /// Socket path
-        path: PathBuf,
-    },
+    Connect(ClientOpts),
+}
+
+#[derive(Debug, StructOpt)]
+struct ListenOpts {
+    /// Socket path
+    path: PathBuf,
+}
+
+#[derive(Debug, StructOpt)]
+struct ClientOpts {
+    /// Socket path
+    path: PathBuf,
+}
+
+async fn cmd_connect(opts: ClientOpts) -> Result<()> {
+    let mut client = Client::connect(opts.path).await?;
+
+    client.spawn_shell().await?;
+
+    Ok(())
+}
+
+async fn cmd_listen(opts: ListenOpts) -> Result<()> {
+    let server = Server::bind(opts.path)?;
+
+    server.handle_incoming_clients().await;
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -35,13 +54,8 @@ fn main() -> Result<()> {
         let opts = Opts::from_args();
 
         match opts {
-            Opts::Listen {
-                path,
-            } => cmd_listen(path).await?,
-
-            Opts::Connect {
-                path,
-            } => cmd_connect(path).await?,
+            Opts::Listen(opts) => cmd_listen(opts).await?,
+            Opts::Connect(opts) => cmd_connect(opts).await?,
         }
 
         Ok(())
